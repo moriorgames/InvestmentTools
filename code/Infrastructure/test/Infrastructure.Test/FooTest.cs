76ec541d65b2389afd80579f1d2c5beba738b960
Morior <moriorgames@gmail.com>
2025-09-01 17:22:42 +0200

    Feature Foo (#1)
    Co-authored-by: Morior <jordi.grados@escapadarural.com>

---
+++ code/Infrastructure/test/Infrastructure.Test/FooTest.cs
@@ -0,0 +1,16 @@
+﻿using Xunit;
+
+namespace Infrastructure.Test;
+
+public class FooTest
+{
+    [Fact]
+    public void ShouldReturnTrue()
+    {
+        var sut = new Foo();
+
+        var actual = sut.Execute();
+
+        Assert.True(actual);
+    }
+}
